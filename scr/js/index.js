b50e6d7bb9e7bef5f0d56bc28ec04b54db018c6f
annxe <122538913+annxe@users.noreply.github.com>
2023-05-16 19:53:33 -0300

    index.js

--- scr/js/index.js
+++ scr/js/index.js
@@ -8,8 +8,8 @@ botaoAlterarTema.addEventListener("click", () => {
   body.classList.toggle("modo-escuro");
 
   if (modoEscuroEstaAtivo) {
-    imagemBotaoTrocaDeTema.setAttribute("src", "./imagens/sun.png");
+    imagemBotaoTrocaDeTema.setAttribute("src", "./scr/imagens/sun.png");
   } else {
-    imagemBotaoTrocaDeTema.setAttribute("src", "./imagens/moon.png");
+    imagemBotaoTrocaDeTema.setAttribute("src", "./scr/imagens/moon.png");
   }
 });
